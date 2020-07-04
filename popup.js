1f70998e0defea070fb410fa7753e07d8b306c9d
Pablo Manoel <pablo.manoel@mobicare.com.br>
2020-07-04 17:40:39 -0300

    - segmentando mensagens por data - filtrando por nome do usuario - key como hora, value como mensagem

--- popup.js
+++ popup.js
@@ -17,6 +17,7 @@ changeColor.onclick = function () {
 				dialogTag = document.getElementsByClassName("SvOPqd")[0].childNodes;
 
 				var currentHeader = '';
+				var responseIndex = 0;
 				var response = {};
 				var boxId = 0;
 
@@ -29,21 +30,50 @@ changeColor.onclick = function () {
 						let headerContent = dialogTag[dialogIndex].textContent;
 
 						currentHeader = headerContent;
-						response[headerContent] = {};
 						
 					} else {
-						let currentHeaderNode = response[currentHeader];
-						currentHeaderNode[boxId] = "boxContent";
-						boxId++;
+						let boxContent = getBoxContent(dialogTag[dialogIndex]);
+						let headerKey = responseIndex.toString().concat(' - ').concat(currentHeader);
+
+						if (!objectIsEmpty(boxContent)){
+							response[headerKey] = boxContent;
+							responseIndex ++;
+						}
+						
 					}					
+				}
+
+				function getBoxContent(box){
+					// [NAO ALTERAR]: pega a lista de mensagens dentro do box
+					// console.log(box.children[1].children[1].children.length);
+
+					let profileBoxes = box.getElementsByClassName('oGsu4');
+					
+					let boxContent = {}
+					for (let index = 0; index < profileBoxes.length; index++) {
+						// nome e hora: profileBoxes[index].childNodes[0]
+						// mensagem: profileBoxes[index].childNodes[1]
+
+						let nameAndHour = profileBoxes[index].childNodes[0].textContent;
+						
+						let hour = nameAndHour.split(' ').filter(splitItem => splitItem.includes(':'))[0].replace(',','');
+						let message = profileBoxes[index].childNodes[1].textContent;
+
+						if (nameAndHour.includes("Mauricio Santos")){
+							boxContent[hour] = message;
+						}						
+					}
+					return boxContent;
+				}
+
+				function objectIsEmpty(obj){
+					return Object.keys(obj).length === 0 && obj.constructor === Object
 				}
 
 				console.log(response);
 			`});
 	});
 };
-
-
 
 
 
