c2bc7efb2199b89cc6eaccd2ba48087cf3a4e8c6
Pablo Manoel <pablo.manoel@mobicare.com.br>
2020-07-04 14:22:50 -0300

    logando cabeçalho e box de mensagens no console do browser

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(function () {
+	chrome.storage.sync.set({ targetChat: null }, function () {});
+
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+		chrome.declarativeContent.onPageChanged.addRules([{
+			conditions: [new chrome.declarativeContent.PageStateMatcher({
+				pageUrl: { hostEquals: 'chat.google.com' },
+			})
+			],
+			actions: [new chrome.declarativeContent.ShowPageAction()]
+		}]);
+	});
+});
---
+++ popup.js
@@ -0,0 +1,52 @@
+let changeColor = document.getElementById('changeColor');
+let okid = document.getElementById('okid');
+// chrome.storage.sync.get('color', function (data) {
+// 	changeColor.style.backgroundColor = data.color;
+// 	changeColor.setAttribute('value', data.color);
+// });
+
+changeColor.onclick = function () {
+	var bkg = chrome.extension.getBackgroundPage();
+
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		chrome.tabs.executeScript(
+			tabs[0].id,
+			{
+				code: `
+				// pega lista de datas e box de mensagens referentes aquela data, lista unica de 1 nivel
+				dialogTag = document.getElementsByClassName("SvOPqd")[0].childNodes;
+				for (let dialogIndex = 0; dialogIndex < dialogTag.length; dialogIndex++) {
+
+					// classes [A2BXPe n5uYMe pYTmjf mCOR5e] determinam o cabeçalho do dia
+					let headerNode = 'A2BXPe n5uYMe pYTmjf mCOR5e';
+
+					if (dialogTag[dialogIndex].classList.toString() === headerNode){
+						console.log(dialogTag[dialogIndex].textContent);
+					} else {
+						console.log('box de mensagens');
+					}					
+				}
+			`});
+	});
+};
+
+
+
+
+
+
+// chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+
+// 	chrome.tabs.executeScript(
+// 		tabs[0].id,
+// 		{ code: "document.body.getElementsByClassName(\"QQodff\")" },
+// 		function (result) {
+// 			bkg.console.log(result);
+// 			// chrome.storage.sync.set({ targetChat: result }, function () {
+// 			// 	bkg.console.log(result);
+// 			// })
+// 			// for (let resultItem of result) {
+// 			// 	bkg.console.log(resultItem);					
+// 			// }
+// 		});
+// });
